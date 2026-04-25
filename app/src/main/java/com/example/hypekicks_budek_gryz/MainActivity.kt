320cf06254b1a5bcaa3ccae4b1c273d6ae20b25a
Mateusz <mateusz.budek@cyfroweszkoly.pl>
2026-04-25 19:01:01 +0200

    work on Main Activity

--- app/src/main/java/com/example/hypekicks_budek_gryz/MainActivity.kt
+++ app/src/main/java/com/example/hypekicks_budek_gryz/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.WindowInsetsCompat
 import com.example.hypekicks_budek_gryz.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
@@ -30,12 +31,13 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        //seedDataBase()
+        seedDataBase()
 
         allSneakersList = mutableListOf()
         sneakersList = mutableListOf()
         adapter = SneakerAdapter(this, sneakersList)
         binding.sneakersGridView.adapter = adapter
+
 
         fetchDataFromCloud()
 
@@ -43,44 +45,56 @@ class MainActivity : AppCompatActivity() {
             val clickedItem = sneakersList[position]
             Toast.makeText(this, "But ${clickedItem.modelName}!", Toast.LENGTH_SHORT).show()
         }
+
+        binding.sneakersSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterSneakers(newText ?: "")
+                return true
+            }
+        })
+
     }
 
     private fun seedDataBase() {
         val sneakers = listOf(
             Sneaker(
                 brand = "Nike",
-                modelName = "Air Jordan 1 Retro High OG 'Chicago'",
+                modelName = "Air Force",
                 resellPrice = 1800f,
                 releaseYear = "2015",
-                imageUrl = ""
+                imageUrl = "https://i.postimg.cc/dVWR6RCv/AIR-FORCE-1-07.png"
             ),
             Sneaker(
                 brand = "Adidas",
-                modelName = "Yeezy Boost 350 V2 'Zebra'",
+                modelName = "Yeezy Boost",
                 resellPrice = 1200f,
                 releaseYear = "2017",
-                imageUrl = ""
+                imageUrl = "https://i.postimg.cc/JhGN2y3h/Adidas-Yeezy-Boost-350-V2-Zebra-Product-1.png"
             ),
             Sneaker(
                 brand = "New Balance",
                 modelName = "990v3 'Grey'",
                 resellPrice = 900f,
                 releaseYear = "2012",
-                imageUrl = ""
+                imageUrl = "https://i.postimg.cc/0NbYB6Y4/1653919886289-New-Balance0.png"
             ),
             Sneaker(
                 brand = "Puma",
                 modelName = "MB.01 'Red Blast'",
                 resellPrice = 700f,
                 releaseYear = "2021",
-                imageUrl = ""
+                imageUrl = "https://i.postimg.cc/jjmH0svC/puma-red-blast.png"
             ),
             Sneaker(
                 brand = "Reebok",
                 modelName = "Question Mid 'Red Toe'",
                 resellPrice = 850f,
                 releaseYear = "1996",
-                imageUrl = ""
+                imageUrl = "https://i.postimg.cc/HLRwDp33/reebok-redtoe.png"
             )
         )
 
@@ -91,10 +105,10 @@ class MainActivity : AppCompatActivity() {
             db.collection("sneakers")
                 .add(sneaker)
                 .addOnSuccessListener {
-                    Log.d("FIREBASE_TEST", "Dodano buta: ${sneaker.}")
+                    Log.d("FIREBASE_TEST", "Dodano buta: ${sneaker.modelName}")
                 }
                 .addOnFailureListener { e ->
-                    Log.e("FIREBASE_TEST", "Błąd podczas dodawania: ", e)
+                    Log.e("FIREBASE_TEST", "Błąd podczas dodawania: ${sneaker.modelName}", e)
                 }
         }
     }
@@ -104,6 +118,7 @@ class MainActivity : AppCompatActivity() {
         db.collection("sneakers")
             .get()
             .addOnSuccessListener { documents ->
+                allSneakersList.clear()
                 sneakersList.clear()
 
                 for (document in documents) {
@@ -114,15 +129,30 @@ class MainActivity : AppCompatActivity() {
                     val imageUrl = document.getString("imageUrl") ?: ""
 
                     val sneaker = Sneaker(brand, modelName, resellPrice, releaseYear, imageUrl)
-                    sneakersList.add(sneaker)
+                    allSneakersList.add(sneaker)
                 }
+                sneakersList.addAll(allSneakersList)
 
-                // MAGIA: Mówimy Adapterowi "Hej, mam nowe dane! Odśwież ekran!"
                 adapter.notifyDataSetChanged()
             }
             .addOnFailureListener { ex ->
                 Log.e("FIREBASE_ERROR", "Błąd pobierania danych: ", ex)
                 Toast.makeText(this, "Błąd pobierania danych z chmury!", Toast.LENGTH_LONG).show()
             }
+    }
+
+    private fun filterSneakers(query: String){
+        sneakersList.clear()
+
+        if (query.isEmpty()) {
+            sneakersList.addAll(allSneakersList)
+        } else {
+            for (sneaker in allSneakersList) {
+                if (sneaker.brand.lowercase().contains(query.lowercase())) {
+                    sneakersList.add(sneaker)
+                }
+            }
+        }
+        adapter.notifyDataSetChanged()
     }
 }
