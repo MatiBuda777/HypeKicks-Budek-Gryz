3de5db0a3f04eac64d6d87f13ba599300c5e6b2e
jurekg <jurek.gryz2007@gmail.com>
2026-04-26 20:14:27 +0200

    Load sneakers in AdminPanel (Read)

--- app/src/main/java/com/example/hypekicks_budek_gryz/AdminPanelActivity.kt
+++ app/src/main/java/com/example/hypekicks_budek_gryz/AdminPanelActivity.kt
@@ -1,20 +1,43 @@
 package com.example.hypekicks_budek_gryz
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AdminPanelActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+    private val db = FirebaseFirestore.getInstance()
+
+    private val sneakers = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_admin_panel)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        listView = findViewById(R.id.adminListView)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, sneakers)
+        listView.adapter = adapter
+
+        loadSneakers()
+    }
+
+    private fun loadSneakers() {
+        db.collection("sneakers")
+            .get()
+            .addOnSuccessListener { result ->
+                sneakers.clear()
+
+                for (doc in result) {
+                    val brand = doc.getString("brand") ?: ""
+                    val model = doc.getString("modelName") ?: ""
+                    sneakers.add("$brand - $model")
+                }
+
+                adapter.notifyDataSetChanged()
+            }
     }
 }
