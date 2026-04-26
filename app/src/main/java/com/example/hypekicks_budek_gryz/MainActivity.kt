97df4cd179ee77f9a66465bad48f4c08b985b7a8
jurekg <jurek.gryz2007@gmail.com>
2026-04-26 23:18:22 +0200

    Fetching data fix

--- app/src/main/java/com/example/hypekicks_budek_gryz/MainActivity.kt
+++ app/src/main/java/com/example/hypekicks_budek_gryz/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-//        seedDataBase()
+        seedDataBase()
 
         allSneakersList = mutableListOf()
         sneakersList = mutableListOf()
@@ -115,31 +115,52 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun fetchDataFromCloud() {
+
+        Log.d("FIREBASE_DEBUG", "Start pobierania danych z Firestore...")
+
         db.collection("sneakers")
             .get()
             .addOnSuccessListener { documents ->
+
+                Log.d("FIREBASE_DEBUG", "Pobrano dokumentów: ${documents.size()}")
+
                 allSneakersList.clear()
                 sneakersList.clear()
 
                 for (document in documents) {
+
+                    Log.d("FIREBASE_DEBUG", "Dokument ID: ${document.id}")
+                    Log.d("FIREBASE_DEBUG", "Dane dokumentu: ${document.data}")
+
                     val brand = document.getString("brand") ?: "Nieznana marka"
                     val modelName = document.getString("modelName") ?: "Nieznany model"
-                    val resellPrice = document.getLong("resellPrice")?.toFloat() ?: 0F
+                    val resellPrice = document.getDouble("resellPrice")?.toFloat() ?: 0F
                     val releaseYear = document.getString("releaseYear") ?: ""
                     val imageUrl = document.getString("imageUrl") ?: ""
 
-                    val sneaker = Sneaker(brand, modelName, resellPrice, releaseYear, imageUrl)
+                    val sneaker = Sneaker(
+                        brand = brand,
+                        modelName = modelName,
+                        resellPrice = resellPrice,
+                        releaseYear = releaseYear,
+                        imageUrl = imageUrl
+                    )
+
                     allSneakersList.add(sneaker)
                 }
-                sneakersList.addAll(allSneakersList)
 
+                sneakersList.addAll(allSneakersList)
                 adapter.notifyDataSetChanged()
+
+                Log.d("FIREBASE_DEBUG", "Lista po pobraniu: ${sneakersList.size} elementów")
             }
             .addOnFailureListener { ex ->
-                Log.e("FIREBASE_ERROR", "Błąd pobierania danych: ", ex)
+                Log.e("FIREBASE_ERROR", "Błąd pobierania danych: ${ex.message}", ex)
                 Toast.makeText(this, "Błąd pobierania danych z chmury!", Toast.LENGTH_LONG).show()
             }
     }
+
+
 
     private fun filterSneakers(query: String){
         sneakersList.clear()
